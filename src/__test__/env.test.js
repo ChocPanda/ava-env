4884b0d4e2111632e472f95bbdb9c9408467ec39
ChocPanda <mattsearle@ymail.com>
2019-05-04 11:57:01 +0100

    test(env): Add a unit test for the restoration of the process.env

--- src/__test__/env.test.js
+++ src/__test__/env.test.js
@@ -1,13 +1,20 @@
-const ava = require('ava'); // eslint-disable-line ava/use-test
-const test = require('../env')(ava);
+const deepClone = require('lodash.clonedeep');
+const test = require('../env')(require('ava'));
 
-test('Ava-env - Should add variables to process.env', t => {
+const originalProcessEnv = deepClone(process.env);
+
+test.serial('Ava-env - Should add variables to process.env', t => {
 	t.context.env({ testEnvVar: 'testValue' });
 	t.is(process.env.testEnvVar, 'testValue');
 });
 
-test('Ava-env - Should overwrite process.env variables', t => {
+test.serial('Ava-env - Should overwrite process.env variables', t => {
 	process.env.testEnvVar = 'testValue1';
 	t.context.env({ testEnvVar: 'differentValue' });
 	t.is(process.env.testEnvVar, 'differentValue');
+});
+
+test.serial('Ava-env - Should restore the original process.env', t => {
+	t.is(process.env.testEnvVar, undefined);
+	t.deepEqual(process.env, originalProcessEnv);
 });
